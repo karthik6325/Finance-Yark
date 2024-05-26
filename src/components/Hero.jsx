84c3139a8f3006757eca2d738e1f9da069b3ccd3
karthik <karthik63254@gmail.com>
2024-05-26 14:07:33 +0530

    fixed bug in navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import BackgroundImage from '../assets/undraw_Finance_re_gnv2.png'; // Ensure the correct path and file extension
+import BackgroundImage from '../assets/undraw_Finance_re_gnv2.png'; 
 import Typed from 'react-typed';
 
 
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <div className='relative text-white'>
       <div className='absolute inset-0 bg-black opacity-20' style={{ backgroundImage: `url(${BackgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
-      <div className='max-w-[900px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center relative z-10'>
+      <div className='max-w-[900px] w-full h-screen mx-auto text-center flex flex-col justify-center relative z-10'>
         <h1 className='text-[#E9D06C] md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>
           Expert financial planning 
         </h1>
@@ -24,7 +24,7 @@ const Hero = () => {
             Every smart investor deserves to work with smart tools.
           </p>
         </div>
-        <button className="bg-[#E9D06C] w-[200px] border-[#E9D06C] rounded-md font-medium my-6 mx-auto py-3 text-black before:ease relative h-12 w-40 overflow-hidden border bg-[#E9D06C]-500 shadow-4xl transition-all before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-60 before:duration-700 hover:shadow-[#E9D06C]-500 hover:before:-translate-x-40">
+        <button className="bg-[#E9D06C] w-[200px] border-[#E9D06C] rounded-md font-medium my-6 mx-auto py-3 text-black before:ease relative h-12 w-40 overflow-hidden border bg-[#E9D06C]-500 text-white shadow-4xl transition-all before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-60 before:duration-700 hover:shadow-[#E9D06C]-500 hover:before:-translate-x-40">
           <span relative="relative z-10">Get Started</span>
         </button>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
+  useEffect(()=>{
+    console.log(nav);
+  },[nav]);
 
   const handleNav = () => {
     setNav(!nav);
   };
 
   return (
-    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-      <h1 className='w-full text-3xl font-bold text-[#E9D06C]'>Yark</h1>
+    <div className='flex justify-between items-center h-24 px-4 text-white w-full bg-white bg-opacity-20'>
+      <h1 className='w-full text-3xl font-bold text-[#E9D06C] ml-6'>Yark</h1>
       <ul className='hidden md:flex'>
         <li className='p-4 font-bold'>Home</li>
         <li className='p-4 font-bold'>About</li>
         <li className='p-4 font-bold'>Services</li>
         <li className='p-4 font-bold'>Tools</li>
-        <li className='p-4 font-bold'>Contact</li>
+        <li className='p-4 font-bold mr-10'>Contact</li>
       </ul>
-      <div onClick={handleNav} className='block md:hidden'>
+      <div onClick={handleNav} className='block md:hidden mr-6'>
           {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
       </div>
-      <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>Yark</h1>
+      <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-grey-900 bg-black ease-in-out duration-500 z-20' : 'ease-in-out duration-500 fixed left-[-100%]'}>
+        <h1 className='w-full text-3xl font-bold text-[#E9D06C] m-4'>Yark</h1>
           <li className='p-4 border-b border-gray-600 font-bold'>Home</li>
           <li className='p-4 border-b border-gray-600 font-bold'>About</li>
           <li className='p-4 border-b border-gray-600 font-bold'>Services</li>
