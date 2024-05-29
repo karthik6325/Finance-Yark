f6153c60bece476be427ad9a6b609874f4fb4a17
karthik <karthik63254@gmail.com>
2024-05-30 00:49:09 +0530

    removed dead code

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
-import Analytics from '../components/Analytics';
-import Cards from '../components/Cards';
 import Footer from '../components/Footer';
-import Hero from '../components/Hero';
-import Planning from '../components/Planning';
 import ServiceCard from '../components/pageComponents/servicePage/ServiceData';
-import SideHero from '../components/SideHero';
-import BackgroundImage from '../assets/undraw_Finance_re_gnv2.png';
 
 function Services() {
   return (
