2fb359ac8ecbd424e3c31bf8c2140bb4ec071a25
karthik <karthik63254@gmail.com>
2024-06-04 01:04:11 +0530

    removed dead code

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
-// Routes.jsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
